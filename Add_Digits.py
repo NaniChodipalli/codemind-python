0054ff8067c96f3c72afea9ff070b947f822c61b
NaniChodipalli <97425897+NaniChodipalli@users.noreply.github.com>
2022-07-13 14:48:48 +0530

    n=int(input()) def add(n):     return (n-1)%9+1 if n>0 else 0 print(add(n))

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,18 +1,4 @@
 n=int(input())
-sum1=0
-sum2=0
-sum3=0
-while n>0:
-    r=n%10
-    n=n//10
-    sum1=sum1+r
-while sum1>0:
-    r1=sum1%10
-    sum1=sum1//10
-    sum2=sum2+r1
-while sum2>0:
-    r2=sum2%10
-    sum2=sum2//10
-    sum3=sum3+r2
-print(sum3)
-    
+def add(n):
+    return (n-1)%9+1 if n>0 else 0
+print(add(n))

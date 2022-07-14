e59f1a1ea3f2eeb86045d1ee9bc7b5ea761d25a3
NaniChodipalli <97425897+NaniChodipalli@users.noreply.github.com>
2022-07-14 21:50:26 +0530

    a,b=map(int,input().split()) gcd=0 for i in range(1,a and b):     if a%i==0 and b%i==0:         gcd=i print(gcd)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,6 @@
-i,j=map(int,input().split())
-while(i!=j):
-  if(i>j):
-     i=i-j
-  else:
-     j=j-i
-print(i)
+a,b=map(int,input().split())
+gcd=0
+for i in range(1,a and b):
+    if a%i==0 and b%i==0:
+        gcd=i
+print(gcd)

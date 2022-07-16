0a63e36f3f4f52d25b4156d50fbbc26989533e2d
NaniChodipalli <97425897+NaniChodipalli@users.noreply.github.com>
2022-07-16 20:07:46 +0530

    x = list(map(str,input().lower().split())) n = [] s=0 l = ['a','e','i','o','u'] for i in x:     for j in i:         if j in l:             s+=1     n.append(s)     s=0 print(min(n))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,11 @@
+x = list(map(str,input().lower().split()))
+n = []
+s=0
+l = ['a','e','i','o','u']
+for i in x:
+    for j in i:
+        if j in l:
+            s+=1
+    n.append(s)
+    s=0
+print(min(n))
